91dae86b1151781ab29f88ec6cc66449e626e828
Rossenant <88676312+Rozzenant@users.noreply.github.com>
2022-12-17 04:14:04 +0300

    Delete RK2/LR3 directory

--- RK2/LR3/gen_random.py
+++
@@ -1,7 +0,0 @@
-from random import randint as rand
-def gen_random(num_count, begin, end):
-    for i in range(num_count):
-        yield rand(begin, end)
-
-if __name__ == "__main__":
-    print(*gen_random(5, 1, 30))
